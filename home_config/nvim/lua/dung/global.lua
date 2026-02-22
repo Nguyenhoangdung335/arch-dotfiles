b0da298ad0eaa028cb20a5c679ef61c10ec12a3b
Nguyenhoangdung335 <nguyenhoangdung335@gmail.com>
2026-02-22 13:17:12 +0700

    feat: add global variable for termux

--- home_config/nvim/lua/dung/global.lua
+++ home_config/nvim/lua/dung/global.lua
@@ -1,4 +1,23 @@
 -- ./lua/dung/global.lua
+
+local is_termux = vim.env.PREFIX and vim.env.PREFIX:match("com.termux") ~= nil
+vim.g.is_termux = is_termux
+
+if is_termux then
+  vim.g.clipboard = {
+    name = "termux",
+    copy = {
+      ["+"] = "termux-clipboard-set",
+      ["*"] = "termux-clipboard-set",
+    },
+    paste = {
+      ["+"] = "termux-clipboard-get",
+      ["*"] = "termux-clipboard-get",
+    },
+    cache_enabled = 0,
+  }
+end
+
 local M = {}
 
 function M.notify(msg, level, opts)
