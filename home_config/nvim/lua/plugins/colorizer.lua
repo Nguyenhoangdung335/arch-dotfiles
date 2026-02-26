7472e04d371b85fd0b54ec7e6a5ec9ea09b908fa
Nguyen Hoang Dung <nguyenhoangdung335@gmail.com>
2026-02-26 12:05:04 +0700

    feat: replace mini.hipatterns for nvim-highlight-colors
    - Replace the old mini.hipatterns with nvim-highlight-colors for better
      support of tailwind colors and other color functions

--- home_config/nvim/lua/plugins/colorizer.lua
+++ home_config/nvim/lua/plugins/colorizer.lua
@@ -1,42 +1,29 @@
 return {
-	"echasnovski/mini.hipatterns",
+	"brenoprata10/nvim-highlight-colors",
 	event = { "BufReadPost", "BufNewFile" },
-	opts = function()
-		local hipatterns = require("mini.hipatterns")
-
-		return {
-			highlighters = {
-				-- 1. Built-in hex color highlighting
-				hex_color = hipatterns.gen_highlighter.hex_color(),
-
-				-- 2. Built-in comment keyword highlighting (New!)
-				todo = hipatterns.gen_highlighter.todo(),
-				fixme = hipatterns.gen_highlighter.fixme(),
-				hack = hipatterns.gen_highlighter.hack(),
-				note = hipatterns.gen_highlighter.note(),
-
-				-- 3. Your custom CSS function highlighter
-				css_color_fn = {
-					pattern = "([%w_]+%(.-%))",
-					hl_group = function(match)
-						local color_name = match:match("^(%w+)")
-						if
-							color_name == "rgb"
-							or color_name == "rgba"
-							or color_name == "hsl"
-							or color_name == "hsla"
-						then
-							return "MiniHipatternsCssFn"
-						end
-					end,
-				},
-			},
-		}
-	end,
-	config = function(_, opts)
-		-- Define the fallback highlight group for css functions
-		vim.api.nvim_set_hl(0, "MiniHipatternsCssFn", { bg = "#3d4256" })
-
-		require("mini.hipatterns").setup(opts)
-	end,
+	opts = {
+		render = "background", -- 'background', 'foreground', 'virtual'
+		virtual_symbol = "■",
+		virtual_symbol_prefix = " ",
+		virtual_symbol_suffix = "",
+		virtual_symbol_position = "inline", -- 'inline', 'eol', 'eow'
+		enable_hex = true,
+		enable_short_hex = true,
+		enable_rgb = true,
+		enable_hsl = true,
+		enable_ansi = true,
+		enable_xterm256 = true,
+		enable_xtermTrueColor = true,
+		enable_hsl_without_function = true,
+		enable_var_usage = true,
+		enable_named_colors = true,
+		enable_tailwind = true,
+		custom_colors = {},
+		exclude_buftypes = {},
+		exclude_filetypes = {},
+		exclude_buffer = function() end,
+	},
+	keys = {
+		{ "<leader>hl", "<cmd>HighlightColors Toggle<cr>", desc = "Toggle highlight colors" },
+	},
 }
