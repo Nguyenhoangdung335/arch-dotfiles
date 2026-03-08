d16cc4330e8b5bea4e28352c60903f119e53f446
Nguyen Hoang Dung <nguyenhoangdung335@gmail.com>
2026-03-08 15:16:35 +0700

    refactor: enable markview for normal markdown and render-markdown for AI

--- home_config/.config/nvim/lua/plugins/markdown.lua
+++ home_config/.config/nvim/lua/plugins/markdown.lua
@@ -6,12 +6,12 @@ return {
 			anti_conceal = { enabled = false },
 			-- file_types = { "opencode_output", "codecompanion" },
 		},
-		ft = { "markdown", "Avante", "copilot-chat", "opencode_output", "codecompanion" },
+		ft = { "Avante", "copilot-chat", "opencode_output", "codecompanion" },
 	},
 	{
 		"OXY2DEV/markview.nvim",
 		ft = "markdown",
-		cond = not vim.g.vscode and false,
+		cond = not vim.g.vscode,
 		opts = {
 			preview = {
 				filetypes = { "markdown" },
