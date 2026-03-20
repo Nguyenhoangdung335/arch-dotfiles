d6719ce8687b65d38af15c571bee08f6f35d9829
Nguyen Hoang Dung <nguyenhoangdung335@gmail.com>
2026-03-20 15:43:23 +0700

    feat(QS-BE-Network): add NMState and NMConnectivity property enum

--- home_config/.config/quickshell/Backend/src/modules/network/enums.rs
+++ home_config/.config/quickshell/Backend/src/modules/network/enums.rs
@@ -1,4 +1,25 @@
 #[derive(Debug, PartialEq)]
+pub enum NMState {
+    Unknown = 0,
+    Asleep = 10,
+    Disconnected = 20,
+    Disconnecting = 30,
+    Connecting = 40,
+    ConnectedLocal = 50,
+    ConnectedSite = 60,
+    ConnectedGlobal = 70,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum NMConnectivity {
+    Unknown = 0,
+    None = 1,
+    Portal = 2,
+    Limited = 3,
+    Full = 4,
+}
+
+#[derive(Debug, PartialEq)]
 pub enum NMDeviceType {
     Unknown = 0,
     Ethernet = 1,
