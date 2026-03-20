8008cd21916950cac32f4d5a44ba68c3c0ce4ed4
Nguyen Hoang Dung <nguyenhoangdung335@gmail.com>
2026-03-20 12:07:04 +0700

    feat(QS-BE-Core): add tracing subscription
    Add tracing subscription to output log into console screen and receive
    optional RUST_LOG environment variable

--- home_config/.config/quickshell/Backend/src/main.rs
+++ home_config/.config/quickshell/Backend/src/main.rs
@@ -9,9 +9,15 @@ use std::error::Error;
 use std::sync::Arc;
 
 use tokio::sync::RwLock;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer()) // Output to console
+        .with(tracing_subscriber::EnvFilter::from_default_env()) // Enable RUST_LOG
+        .init();
+
     let cfg = Arc::new(RwLock::new(config::load_or_create_config().await?));
     let sys_bus = zbus::Connection::system().await?;
     // Uncomment when used for other services
