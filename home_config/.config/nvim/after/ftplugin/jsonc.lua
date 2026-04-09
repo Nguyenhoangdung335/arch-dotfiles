45d7f8bc107df68f0abc48f941a853c6d501edf5
Nguyen Hoang Dung <nguyenhoangdung335@gmail.com>
2026-04-09 11:02:22 +0700

    feat(neovim): add jsonc formatters and custom options for jsonc filetype

---
+++ home_config/.config/nvim/after/ftplugin/jsonc.lua
@@ -0,0 +1,6 @@
+-- 2 spaces for tab
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.expandtab = true -- Use spaces instead of tabs
+vim.opt_local.smartindent = true
